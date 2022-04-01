78c218c92ea75d5ad51f87398a540642055c9867
Kevin Feng <kfengc27@gmail.com>
2022-04-01 14:35:48 +1100

    add files

--- echo-client.py
+++ echo-client.py
@@ -1,13 +1,32 @@
 # echo-client.py
 
 import socket
+import rsa
+
+# Source: https://www.geeksforgeeks.org/how-to-encrypt-and-decrypt-strings-in-python/
+
+# generate public and private keys with
+# rsa.newkeys method,this method accepts
+# key length as its parameter
+# key length should be atleast 16
+publicKey, privateKey = rsa.newkeys(512)
+
+# rsa.encrypt method is used to encrypt
+# string with public key string should be
+# encode to byte string before encryption
+# with encode method
+
+passport = 'PO'
+
+encMessage = rsa.encrypt(passport.encode(),
+                         publicKey)
 
 HOST = "127.0.0.1"  # The server's hostname or IP address
 PORT = 65432  # The port used by the server
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.connect((HOST, PORT))
-    s.sendall(b"Hello, world")
+    s.sendall(b"Hello World")
     data = s.recv(1024)
 
 print(f"Received {data!r}")
--- echo-server.py
+++ echo-server.py
@@ -1,6 +1,13 @@
 # echo-server.py
 
 import socket
+import rsa
+
+# generate public and private keys with
+# rsa.newkeys method,this method accepts
+# key length as its parameter
+# key length should be atleast 16
+publicKey, privateKey = rsa.newkeys(512)
 
 HOST = "127.0.0.1"  # Standard loopback interface address (localhost)
 PORT = 65432  # Port to listen on (non-privileged ports are > 1023)
@@ -13,6 +20,12 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         print(f"Connected by {addr}")
         while True:
             data = conn.recv(1024)
+                        # the encrypted message can be decrypted
+            # with ras.decrypt method and private key
+            # decrypt method returns encoded byte string,
+            # use decode method to convert it to string
+            # public key cannot be used for decryption
+            decMessage = rsa.decrypt(encMessage, privateKey).decode()
             if not data:
                 break
             conn.sendall(data)
